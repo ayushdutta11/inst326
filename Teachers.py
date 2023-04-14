4dbb103e1c4f06077cd2c98bce0419be37f70099
Samuel Castaneda <samcas03@terpmail.umd.edu>
2023-04-14 18:15:56 -0400

    added Teachers class

---
+++ Teachers.py
@@ -0,0 +1,13 @@
+
+class Teacher:
+    
+    
+    def __init__(self, name, students, taughtclass):
+        #Finish implementation later
+        self.students = []
+        self.name = name
+        self.taughtclass = taughtclass
+        #have to iterate through some sort of file to append students to
+        #the teachers students attribute.
+        
+    
